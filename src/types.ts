efe0e0f70d3f422d9f4d8ed87a7a236c5a1bc11c
Devqon <s.geerts@live.nl>
2020-01-24 20:38:52 +0100

    Render different options

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export interface CardConfig {
   type: string;
   entity: string;
   name?: string;
-  dropdown?: 'hvac_modes' | 'preset_modes';
+  dropdown?: 'hvac_modes' | 'preset_modes' | string;
 }
 
 export interface Values {
